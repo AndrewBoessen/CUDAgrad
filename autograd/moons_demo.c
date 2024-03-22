841cfcf2e0da1b86ff024c5b9d1df6e7e50c3d0e
andrew <boessena@bc.edu>
2024-03-22 12:36:37 -0400

    update moons_demo

--- autograd/moons_demo.c
+++ autograd/moons_demo.c
@@ -55,7 +55,7 @@ int main() {
     printf("Loaded %d entries from %s\n", num_entries, filename);
 
     // Init MLP
-    int sizes[] = {NUM_INPUTS, 16, 16, NUM_OUTPUTS};
+    int sizes[] = {NUM_INPUTS, 5, 15, 5, NUM_OUTPUTS};
     int nlayers = sizeof(sizes) / sizeof(int);
 
     MLP* mlp = init_mlp(sizes, nlayers);
